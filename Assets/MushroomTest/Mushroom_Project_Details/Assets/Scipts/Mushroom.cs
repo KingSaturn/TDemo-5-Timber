288c6d881ed03a60ce0898a58a7e8e9e938983f4
Ethan Munday <ethanmunday80@googlemail.com>
2022-04-22 18:37:04 +0100

    Added the mushroom project files into the project.
    No idea if this is finished. But I'm doing it anyways

---
+++ Assets/MushroomTest/Mushroom_Project_Details/Assets/Scipts/Mushroom.cs
@@ -0,0 +1,199 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushroom : MonoBehaviour
+{
+//Mushroom Vairables
+	//Prerequisites, the player must have a "Muhsroom" tag in unity.
+	//The gameObject must have a character controller
+	public static GameObject mushroom;
+		private const string mushroom_tag= "Mushroom";
+		private CharacterController Controller;
+		private float speed= 50;
+		private Vector3 velocity;
+		
+		private bool is_neutral;
+//Animation Block
+	//The player must have an Animator component
+	private Animator mushroom_animations;
+		private int movement_animation_logger;	//Used for keeping track of if the mushroom is moving
+		
+// Start is called before the first frame update
+	void Start()
+	{
+	//PLayer	-Automatically aqquires objects no need to pass them in via public 
+		player=(GameObject.FindGameObjectsWithTag(player_tag ) )[0 ];	//Going to need to be set differently when spawning in the mushroom objects.
+			Controller=player.GetComponent<CharacterController>();
+			mushroom_animations=player.GetComponent<Animator>();
+	//Need to track and set the mushrooms state ie Aggressive or not
+		//Animator value
+		mushroom_animations.SetBool("Is_Neutral", true );	
+		is_neutral= true;
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+//Triggers section, you will replace these if's with AI triggers corrosponding to the actions
+	//Moving Foward
+		if(Input.GetKeyDown(KeyCode.W ) )
+		{
+			movement_animation_logger+= 1;	//When pressed down, it registers the key to be used
+		}
+		
+		else if(Input.GetKeyUp(KeyCode.W ) )
+		{
+			movement_animation_logger-= 1;	//Used to unregister the key, it will decrease the counter
+		}
+		
+		else
+		{
+		}
+	//Moving To The Left
+		if(Input.GetKeyDown(KeyCode.A ) )
+		{
+			movement_animation_logger+= 1;
+		}
+		
+		else if(Input.GetKeyUp(KeyCode.A ) )
+		{
+			movement_animation_logger-= 1;
+		}
+		
+		else
+		{
+		}
+	//Moving To The Right
+		if(Input.GetKeyDown(KeyCode.S ) )
+		{
+			movement_animation_logger+= 1;
+		}
+		
+		else if(Input.GetKeyUp(KeyCode.S ) )
+		{
+			movement_animation_logger-= 1;
+		}
+		
+		else
+		{
+		}
+	//Moving Backwards
+		if(Input.GetKeyDown(KeyCode.D ) )
+		{
+			movement_animation_logger+= 1;
+		}
+		
+		else if(Input.GetKeyUp(KeyCode.D ) )
+		{
+			movement_animation_logger-= 1;
+		}
+		
+		else
+		{
+		}
+	//Attacking
+		if(Input.GetKeyDown(KeyCode.E ) )
+		{
+			if(is_neutral== false )
+			{
+				Debug.Log("Attack" );
+				mushroom_animations.SetTrigger("IsAttacking" );	//Pass in the parameter from the animator window it will trigger to start
+			}
+			
+			else
+			{
+				Debug.Log("Cannot attack MUST TRANSFORM" );
+			}
+		}
+		
+		else
+		{
+		}
+	//Talking
+		if(Input.GetKeyDown(KeyCode.R )&& (movement_animation_logger== 0 ) )
+		{
+			switch(is_neutral )
+			{
+				case false:
+					Debug.Log("Cannot talk" );
+					break;
+				
+				default:
+					Debug.Log("Talk" );
+					mushroom_animations.SetTrigger("IsTalking" );
+					break;
+			}
+		}
+		
+		else
+		{
+		}
+	//Transforming 
+		if(Input.GetKeyDown(KeyCode.T )&& (movement_animation_logger== 0 ) )
+		{
+			switch(is_neutral )
+			{
+				case false:
+					Debug.Log("Cannot transform" );
+					break;
+				
+				default:
+					Debug.Log("Transform" );
+					mushroom_animations.SetBool("Is_Neutral", false );
+					is_neutral= false;
+					
+					mushroom_animations.SetTrigger("Transform" );
+					break;
+			}
+			
+		}
+		
+		else
+		{
+		}
+	//Animation Playing block
+		//Based on inputs to see if the animations can play or not 
+		if(movement_animation_logger== 0 )	//When the counter is no longer 0 it is valid so walking stops
+		{
+			switch(is_neutral )
+			{
+				case false:
+					mushroom_animations.SetBool("IsMoving" ,false );
+					//Debug.Log("Idle but aggr" );
+					break;
+				
+				default:
+					mushroom_animations.SetBool("IsMoving" ,false );
+					//Debug.Log("Plain Idle" );
+					break;
+			}
+		}
+		
+		else
+		{
+			switch(is_neutral )
+			{
+				case false:
+					mushroom_animations.SetBool("IsMoving" ,true );
+					//Debug.Log("Aggr move" );
+					break;
+				
+				default:
+					mushroom_animations.SetBool("IsMoving" ,true );
+					//Debug.Log("Plain move" );
+					break;
+			}
+		}
+		
+	//Movement block
+		float x= Input.GetAxis("Horizontal" );
+		float z= Input.GetAxis("Vertical" );
+		
+		Vector3 move=transform.right* x+ transform.forward* z;
+
+		Controller.Move(move* speed* Time.deltaTime );
+		Controller.Move(velocity* Time.deltaTime );	
+	}
+}
