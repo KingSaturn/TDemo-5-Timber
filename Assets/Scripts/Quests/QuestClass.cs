e24c9158ea01466db22097aa68acd6b1b8ab0854
Ethan Munday <ethanmunday80@googlemail.com>
2022-04-25 18:53:44 +0100

    Basis for a quest system.
    Not currently working on it, if we have other programmers they can work on it.

---
+++ Assets/Scripts/Quests/QuestClass.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestClass
+{
+    public string name;
+    public string description;
+    public string completed;
+    public string type;
+}
