31acdfda2c15a2063d783190573515eb9fb9dc17
Ethan Munday <ethanmunday80@googlemail.com>
2022-04-25 13:21:19 +0100

    Play button now changes from New Game to Continue based on the save

--- Assets/Scripts/Global/PlayButton.cs
+++ Assets/Scripts/Global/PlayButton.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayButton : MonoBehaviour
 {
     private SaveData data;
+    private Text buttonText;
 
     private void Awake()
     {
+        buttonText = this.GetComponentInChildren<Text>();
         if (File.Exists(Path.Combine(Application.persistentDataPath, "/SaveData.txt")))
         {
             string input = File.ReadAllText(Path.Combine(Application.persistentDataPath, "/SaveData.txt"));
             data = JsonUtility.FromJson<SaveData>(input);
+            buttonText.text = "Continue";
+        }
+        else
+        {
+            buttonText.text = "New Game";
         }
     }
 
